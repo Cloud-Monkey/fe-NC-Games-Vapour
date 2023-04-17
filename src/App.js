419ed1bf7325bae5c4fa0e27a5031a059d8d3416
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-04-17 11:24:52 +0100

    changed structure for component files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import './App.css';
-import PageHeader from './components/PageHeader';
+import "./App.css";
+import PageHeader from "./components/PageHeader";
 
 function App() {
   return (
     <div className="App">
-      <PageHeader className="App-header"/>
-      {/* <header className="App-header">NC Games!</header> */}
-
-    
-    
-    
+      <PageHeader className="App-header" />
     </div>
-
   );
 }
 
