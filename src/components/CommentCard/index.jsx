2c640d7073aed66b14632253aa6bf58427041d92
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-04-19 13:48:17 +0100

    small change to comment card

---
+++ src/components/CommentCard/index.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+function CommentCard({ comment }) {
+  console.log(comment, "<------------ Comment Card comments");
+  const { author, body, created_at } = comment;
+  const dateString = new Date(created_at);
+  const formattedDate = dateString.toDateString();
+
+  return (
+    <div className="grid-comment-display">
+      <div className="comment-body">
+        <div className="comment-card-body">comment: {body}</div>
+      </div>
+      <div className="author-comment">
+        <div className="comment-card-author">author: {author}</div>
+      </div>
+      <div className="created-comment">
+        <div className="comment-card-created">{formattedDate}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CommentCard;
